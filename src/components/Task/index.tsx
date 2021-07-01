b96ab4cc2ac5a4260ebdd804df590007121b7f5f
shirajuki <shirajuki00@gmail.com>
2021-07-01 12:22:52 +0200

    fix: fixes task's time to correctly display red, yellow, green status

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -43,9 +43,10 @@ const Task: React.FC<TaskType> = ({
   const getTimeStatusColor = (taskDate: string | undefined) => {
     if (taskDate) {
       const tdate = new Date(prettyDate(date) + " " + taskDate);
-      const date4 = new Date(date.getTime() - 3600 * 1000 * 4); // 4 hours from tdate
-      if (tdate > date) return "#FF7575";
-      else if (tdate > date4) return "#FFE193";
+      const date4 = new Date(date.getTime() + 3600 * 1000 * 4); // 4 hours from tdate
+      console.log(tdate, date4);
+      if (tdate < date) return "#FF7575";
+      else if (tdate < date4) return "#FFE193";
       return "#ABF987";
     }
     return "transparent";
